adbc5065659444e319b1ec154dd53e80d3aa451c
Alex Rudniy <arudniy@drew.edu>
2025-10-22 08:01:29 -0400

    Added minimal app

---
+++ accounts/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('Meta'), {'fields': ('user_type', 'username')}),
+    )
+    add_fieldsets = (
+        (None, {'classes': ('wide',), 'fields': ('email', 'password1', 'password2', 'user_type')}),
+    )
+    list_display = ('email', 'user_type', 'is_staff', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
---
+++ pages/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.welcome, name='welcome'),
+    path('screen1/', views.screen1, name='screen1'),
+    path('screen2/', views.screen2, name='screen2'),
+    path('screen3/', views.screen3, name='screen3'),
+]
