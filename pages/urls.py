db395e028aabbe0b0e2f69faf497cbd25b261bbd
Dylan-Brooks0215 <dbrooks2@drew.edu>
2025-11-10 12:44:20 -0500

    Created debugging branch/ added files

--- pages/urls.py
+++ pages/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
+from . import buggy_view   # <-- add this import
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('screen1/', views.screen1, name='screen1'),
     path('screen2/', views.screen2, name='screen2'),
     path('screen3/', views.screen3, name='screen3'),
-    path('achievements/', views.student_achievements, name='student_achievements'),
-    path('faq/', views.faq, name='faq'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('notifications/', views.notifications, name='notifications'),
+    path('company/home/', views.company_home, name='company_home'),
+    path('buggy/', buggy_view.buggy_search, name='buggy_search'),  # <-- add this route
 ]
