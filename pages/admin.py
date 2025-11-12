4606a38a55768cb3869e8e13ff6f6bd19162b1e6
Your Name <you@example.com>
2025-11-12 10:54:11 -0500

    Add django test admin for Achievement model

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Achievement
 # Register your models here.
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'date_completed')
+    search_fields = ['title', 'description']
+
+admin.site.register(Achievement, AchievementAdmin)
