4a06170b3d9bebdaaf9164025292a848b284da6a
Your Name <you@example.com>
2025-11-14 14:08:11 -0500

    Add home page loads

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class PagesTestCase(TestCase):
+    def test_welcome_page_loads(self):
+        """
+        Simple test to verify that the welcome page can be loaded
+        and returns HTTP 200 OK.
+        """
+        response = self.client.get(reverse('welcome'))
+        self.assertEqual(response.status_code, 200)
